5c4228723a1ebd48539735409eb574d457b6ad6d
ernesto ponce <ernesto@pigmalionstudios.com>
2016-10-08 23:32:31 -0300

    Agrego listado de posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
+        <title></title>
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Posts</h1>
+                    
+                    
+                    <div class="col-sm-12 col-md-6"> 
+                        <div class="thumbnail"> 
+                            <img 
+                            src="http://www.rememberlayne.com/blog/wp-content/uploads/2015/12/55BE2108-alice-in-chains-the-untold-story-book-out-this-week-image.jpg" 
+                            > 
+                            <div class="caption"> 
+                                <h3>Thumbnail label</h3> 
+                                    <p>
+                                        3333 points  -  1000 comments
+                                    </p>
+                                    <a href="#" class="btn btn-default" role="button">
+                                        <span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span>
+                                    </a> 
+                                    <a href="#" class="btn btn-default" role="button">
+                                        <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+                                    </a> 
+                                    
+                                </p>
+                            </div>
+                        </div> 
+                        
+                        <div class="thumbnail"> 
+                            <img 
+                            src="http://www.rememberlayne.com/blog/wp-content/uploads/2015/12/55BE2108-alice-in-chains-the-untold-story-book-out-this-week-image.jpg" 
+                            data-holder-rendered="true"> 
+                            <div class="caption"> <h3>Thumbnail label</h3> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> </div>
+                        </div> 
+                        
+                        <div class="thumbnail"> 
+                            <img 
+                            src="http://www.rememberlayne.com/blog/wp-content/uploads/2015/12/55BE2108-alice-in-chains-the-untold-story-book-out-this-week-image.jpg" 
+                            data-holder-rendered="true"> 
+                            <div class="caption"> <h3>Thumbnail label</h3> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p> </div>
+                        </div> 
+                    </div>
+                </div>
+            
+        
+        </div>
+        
+    </body>
+</html>
